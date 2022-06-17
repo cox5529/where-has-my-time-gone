0ef9deff2ac907bc32a6df401ef926b22ec87117
Brandon Cox <brandon.cox@metova.com>
2022-06-17 13:03:09 -0500

    Add tailwind

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,36 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
-  "root": true,
-  "extends": [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript/recommended",
-    "@vue/eslint-config-prettier"
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript/recommended',
+    '@vue/eslint-config-prettier',
   ],
-  "overrides": [
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    indent: [
+      'warn',
+      2,
+      {
+        offsetTernaryExpressions: true,
+        ignoredNodes: ['ConditionalExpression *'],
+        SwitchCase: 1,
+      },
+    ],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['warn', 'single'],
+    semi: ['error', 'always'],
+    '@typescript-eslint/no-explicit-any': ['off'],
+    '@typescript-eslint/explicit-function-return-type': ['error'],
+  },
+  overrides: [
     {
-      "files": [
-        "cypress/e2e/**.{cy,spec}.{js,ts,jsx,tsx}"
-      ],
-      "extends": [
-        "plugin:cypress/recommended"
-      ]
-    }
-  ]
-}
+      files: ['cypress/e2e/**.{cy,spec}.{js,ts,jsx,tsx}'],
+      extends: ['plugin:cypress/recommended'],
+    },
+  ],
+};
