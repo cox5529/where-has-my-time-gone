82156ed9b25b0c996c72b7f3bd43406e6e2774bd
Brandon Cox <brandon.cox@metova.com>
2022-06-17 13:06:17 -0500

    Add import sorter

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,4 +3,10 @@ module.exports = {
   semi: true,
   singleQuote: true,
   jsxSingleQuote: true,
+  importOrder: ['^react$', '^[^\\.]*$', '^((\\./)|(\\.\\./))+[^\\.]+$', '^\\..*.css$', '^(\\./)[^\\.]+.*$', '^.*$'],
+  importOrderSeparation: true,
+  importOrderParserPlugins: [
+    "typescript",
+    "[\"decorators\", { \"decoratorsBeforeExport\": true }]"
+  ]
 };
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
-import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { createApp } from 'vue';
+
+import router from './router';
+
 import './index.css';
 
 import App from './App.vue';
-import router from './router';
 
 const app = createApp(App);
 
